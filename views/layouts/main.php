2af47982e41e6fe8418673272d6818b5581027ca
liverpoolfc88 <52122299+liverpoolfc88@users.noreply.github.com>
2020-05-22 11:05:55 +0500

    sdf
    sdfsd

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,9 +38,9 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Bosh sahifa', 'url' => ['/site/index']],
+            ['label' => 'Biz haqimizda', 'url' => ['/site/about']],
+            ['label' => 'Aloqa', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
